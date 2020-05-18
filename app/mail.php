6536371080a96ce6469b483013da9cbedeb6a311
Oleg <frontendercode@gmail.com>
2020-05-18 19:49:58 +0300

    v.0.42
    Настроил работу многостраничного сайта, удалил ненужные скрипты

--- app/mail.php
+++ app/mail.php
@@ -6,14 +6,15 @@ $subject   = "Заказ с сайта https://frontend-coder.github.io/";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
-$messagesite = trim($_POST["messagesite"]);
-
-
+$formaone = trim($_POST["formaone"]);
+$admindata = trim($_POST["admin-data"]);
 
 $message = "
-Имя обратившегося: $name <br>
-Почтовый ящик клиента: $email <br>
-Текст сообщения: $messagesite";
+С какого сайта пришло сообщение: $sitename  <br>
+Место размещения формы в шаблоне: $formaone  <br>
+Место размещения во всплывающей форме: $admindata  <br>
+Имя обратившегося: $name  <br>
+Почтовый ящик клиента: $email";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $subject, $message, "Content-type: text/html; charset=\"utf-8\"\n From: $recepient");
